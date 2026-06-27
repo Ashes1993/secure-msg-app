7d5de0cbc8e4888207f2708a6ff7ee2715bbe0ed
Ashkan <ashkan.eddyking@gmail.com>
2026-06-27 20:02:42 +0330

    feat(actions): implement getRooms query with last-message optimization and sorting

--- client/src/actions/room.ts
+++ client/src/actions/room.ts
@@ -8,6 +8,15 @@ interface SanitizedRoom {
   id: string;
 }
 
+interface SidebarRoom {
+  id: string;
+  type: string;
+  updatedAt: Date;
+  targetUserId: string;
+  targetUserUsername: string;
+  lastMessage: string;
+}
+
 export async function createRoom(
   targetUserId: string,
 ): Promise<ActionResponse<SanitizedRoom | null>> {
@@ -62,6 +71,90 @@ export async function createRoom(
       success: false,
       error:
         "Encountered an internal error while building the messagin channel.",
+      data: null,
+    };
+  }
+}
+
+export async function getRooms(): Promise<
+  ActionResponse<SidebarRoom[] | null>
+> {
+  const session = await auth();
+  const currentUserId = session?.user?.id;
+
+  if (!currentUserId)
+    return { success: false, error: "User unauthorized.", data: null };
+
+  try {
+    const rooms = await prisma.room.findMany({
+      where: {
+        roomParticipants: {
+          some: {
+            userId: currentUserId,
+          },
+        },
+      },
+
+      orderBy: {
+        updatedAt: "desc",
+      },
+
+      select: {
+        id: true,
+        type: true,
+        updatedAt: true,
+        roomParticipants: {
+          where: {
+            NOT: {
+              userId: currentUserId,
+            },
+          },
+          select: {
+            user: {
+              select: {
+                id: true,
+                username: true,
+              },
+            },
+          },
+        },
+        messages: {
+          orderBy: {
+            createdAt: "desc",
+          },
+          take: 1,
+          select: {
+            content: true,
+            createdAt: true,
+            senderId: true,
+          },
+        },
+      },
+    });
+
+    const sanitizedRooms: SidebarRoom[] = rooms.map((room) => {
+      const targetParticipant = room.roomParticipants[0]?.user;
+      const latestMessage = room.messages[0];
+
+      return {
+        id: room.id,
+        type: room.type,
+        updatedAt: room.updatedAt,
+
+        targetUserId: targetParticipant?.id || "unknown-id",
+        targetUserUsername: targetParticipant?.username || "Unknown User",
+
+        lastMessage: latestMessage?.content || "No messages yet. Say hello!",
+      };
+    });
+
+    return { success: true, error: null, data: sanitizedRooms };
+  } catch (err) {
+    console.log("Database error during rooms retrieving: ", err);
+    return {
+      success: false,
+      error:
+        "Encountered an error when trying to retrieve rooms from the database",
       data: null,
     };
   }
