401901764a0b4c33dfbad9438f4c294d29cc7ae6
Ashkan <ashkan.eddyking@gmail.com>
2026-06-28 00:27:35 +0330

    fix(ui): add box-border to the body for a real sceen height design

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="h-screen flex box-border p-4">
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
