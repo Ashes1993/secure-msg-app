94a7ea529efab1dd1d163388cdfb5c11e3f4b82b
Ashkan <ashkan.eddyking@gmail.com>
2026-06-28 21:31:42 +0330

    feat(sidebar): add logout button

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -2,10 +2,11 @@
 
 import RoomList from "./RoomList";
 import UserDiscovery from "./UserDiscovery";
+import LogoutButton from "../auth/LogoutButton";
 
 export default function Sidebar() {
   return (
-    <aside className=" w-full md:w-1/4 bg-background text-foreground p-4 shadow-md shadow-muted-foreground border-border rounded-xl">
+    <aside className="flex flex-col w-full md:w-1/4 bg-background text-foreground p-4 shadow-md shadow-muted-foreground border-border rounded-xl">
       <h1 className="font-bold text-xl mb-4">Messaging App</h1>
 
       {/* Search section */}
@@ -15,6 +16,9 @@ export default function Sidebar() {
       <RoomList />
 
       {/* Logout */}
+      <div className="mt-auto mx-auto ">
+        <LogoutButton />
+      </div>
     </aside>
   );
 }
