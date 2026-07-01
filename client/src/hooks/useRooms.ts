5ee752aad26930ea89ccb98b2a34990e2654dba9
Ashkan <ashkan.eddyking@gmail.com>
2026-07-01 16:25:28 +0330

    feat(hooks): add encryption process to the last image

--- client/src/hooks/useRooms.ts
+++ client/src/hooks/useRooms.ts
@@ -1,24 +1,89 @@
 "use client";
 
-import { getRooms } from "@/actions/room";
+import { getRooms } from "@/actions/rooms";
 import { useQuery } from "@tanstack/react-query";
+import { decryptMessage } from "@/lib/crypto";
+import { useAuthStore } from "@/stores/useAuthStore";
 
 export function useRooms() {
+  const myPrivateKey = useAuthStore((state) => state.privateKey);
+
   const rooms = useQuery({
     queryKey: ["rooms"],
+
     queryFn: async () => {
-      const response = await getRooms();
-      if (!response.success) {
-        throw new Error(response.error || "Failed to retrieve rooms");
+      if (!myPrivateKey) {
+        throw new Error(
+          "Private key unverified or missing. Please sign in again.",
+        );
       }
 
-      return response.data;
+      const response = await getRooms();
+      if (!response.success) {
+        throw new Error(
+          response.error ||
+            "Failed to retrieve rooms. Please refresh the page.",
+        );
+      }
+
+      const rawRooms = response.data;
+
+      if (!rawRooms) {
+        throw new Error("Failed to retrieve rooms. Please refresh the page.");
+      }
+
+      return Promise.all(
+        rawRooms.map(async (room) => {
+          if (!room.lastMessageSenderId) return room;
+
+          const {
+            lastMessage,
+            lastMessageSenderId,
+            lastMessageIv,
+            lastMessageSenderEncryptedKey,
+            lastMessageRecipientEncryptedKey,
+            currentUserId,
+          } = room;
+
+          if (
+            !lastMessageIv ||
+            !lastMessageSenderEncryptedKey ||
+            !lastMessageRecipientEncryptedKey
+          ) {
+            return {
+              ...room,
+              lastMessage: "Unable to load message.",
+            };
+          }
+          try {
+            const isSender = lastMessageSenderId === currentUserId;
+
+            const payload = {
+              encryptedContent: lastMessage,
+              senderEncryptedKey: lastMessageSenderEncryptedKey,
+              recipientEncryptedKey: lastMessageRecipientEncryptedKey,
+              iv: lastMessageIv,
+            };
+
+            const plaintext = await decryptMessage(
+              payload,
+              myPrivateKey,
+              isSender,
+            );
+
+            return { ...room, lastMessage: plaintext };
+          } catch {
+            return { ...room, lastMessage: "Unable to load message." };
+          }
+        }),
+      );
     },
+    enabled: !!myPrivateKey,
   });
 
   return {
     rooms: rooms.data || [],
-    isLoading: rooms.isLoading,
+    isPending: rooms.isPending,
     error: rooms.error?.message || null,
   };
 }
